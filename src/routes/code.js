8e29e52acb7163d0c08e2d82aa524a9bcdc26f93
vimalgattani <gattanivimal5@gmail.com>
2024-06-29 17:23:50 +0530

    remove js files

--- src/routes/code.js
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-import { initiate, validate } from '../controllers/code.js';
-const router = express.Router();
-
-router.post('/generate', initiate);
-router.post('/validate', validate);
-
-export {router as codeRoutes}
--- src/utils/index.js
+++
@@ -1,3 +0,0 @@
-export const getRandomGeneratedCode = () => {
-    return Math.random().toString().substr(2, 6);
-}
